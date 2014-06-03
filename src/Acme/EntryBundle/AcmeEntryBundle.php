bb53251fe77b3722288f94d696d19035447ff206
jony <habibun.noby@gmail.com>
2014-06-03 15:36:20 +0600

    entry bundle added

---
+++ src/Acme/EntryBundle/AcmeEntryBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Acme\EntryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AcmeEntryBundle extends Bundle
+{
+}
---
+++ src/Acme/EntryBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Acme\EntryBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('AcmeEntryBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
