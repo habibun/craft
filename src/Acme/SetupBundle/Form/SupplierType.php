942629a0d3b83227bc12e69d56ae50d568e98a60
Habibun Noby <habibun.noby@gmail.com>
2014-08-16 10:17:56 +0600

    add supplier

---
+++ src/Acme/SetupBundle/Form/SupplierType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Acme\SetupBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SupplierType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('address')
+            ->add('createdAt')
+            ->add('createdBy')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Acme\SetupBundle\Entity\Supplier'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'acme_setupbundle_supplier';
+    }
+}
