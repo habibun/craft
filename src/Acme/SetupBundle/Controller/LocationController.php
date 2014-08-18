9f14d0d18afd6a0d05221ed901695a5a8ce94522
Habibun Noby <habibun.noby@gmail.com>
2014-08-18 09:59:06 +0600

    setup location added

---
+++ src/Acme/SetupBundle/Controller/LocationController.php
@@ -0,0 +1,243 @@
+<?php
+
+namespace Acme\SetupBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Acme\SetupBundle\Entity\Location;
+use Acme\SetupBundle\Form\LocationType;
+
+/**
+ * Location controller.
+ *
+ */
+class LocationController extends Controller
+{
+
+    /**
+     * Lists all Location entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('AcmeSetupBundle:Location')->findAll();
+
+        $paginator = $this->get('knp_paginator');
+        $entities = $paginator->paginate(
+            $entities,
+            $this->get('request')->query->get('page', 1) /*page number*/,
+            10
+        /*limit per page*/
+        );
+
+        return $this->render('AcmeSetupBundle:Location:index.html.twig', array(
+            'entities' => $entities,
+        ));
+    }
+    /**
+     * Creates a new Location entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Location();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('location_show', array('id' => $entity->getId())));
+        }
+
+        return $this->render('AcmeSetupBundle:Location:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to create a Location entity.
+     *
+     * @param Location $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Location $entity)
+    {
+        $form = $this->createForm(new LocationType(), $entity, array(
+            'action' => $this->generateUrl('location_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Save'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Location entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Location();
+        $form   = $this->createCreateForm($entity);
+
+        return $this->render('AcmeSetupBundle:Location:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Location entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AcmeSetupBundle:Location')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Location entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('AcmeSetupBundle:Location:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Location entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AcmeSetupBundle:Location')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Location entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('AcmeSetupBundle:Location:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+    * Creates a form to edit a Location entity.
+    *
+    * @param Location $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Location $entity)
+    {
+        $form = $this->createForm(new LocationType(), $entity, array(
+            'action' => $this->generateUrl('location_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Location entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AcmeSetupBundle:Location')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Location entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add(
+                'heads_up',
+                "Your change was successfully saved."
+            );
+
+            return $this->redirect($this->generateUrl('location_edit', array('id' => $id)));
+        }
+
+        return $this->render('AcmeSetupBundle:Location:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    /**
+     * Deletes a Location entity.
+     *
+     */
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('AcmeSetupBundle:Location')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Location entity.');
+        }
+
+        try {
+            $em->remove($entity);
+            $em->flush();
+        } catch (\Exception $e) {
+            $this->get('session')->getFlashBag()->set(
+                'error',
+                'Error: You can\'t delete this record. You are getting this message because somewhere you already used this record as reference or this record not exist. If you want to know more please contact system administrator.'
+            );
+
+            return $this->redirect($this->get('request')->server->get('HTTP_REFERER'));
+        }
+        $this->get('session')->getFlashBag()->add('oh_snap', 'Location was successfully deleted.');
+
+        //return $this->redirect($this->get('request')->server->get('HTTP_REFERER'));
+        return $this->redirect($this->generateUrl('location'));
+    }
+
+    /**
+     * Creates a form to delete a Location entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('location_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
+}
