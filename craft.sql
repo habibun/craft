558439c5840467f0d3a8bb77d5810de0bd514df5
Habibun Noby <habibun.noby@gmail.com>
2014-08-20 10:48:35 +0600

    database configure

--- craft.sql
+++ craft.sql
@@ -24,7 +24,7 @@ CREATE TABLE `company` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `company` */
 
@@ -50,32 +50,35 @@ DROP TABLE IF EXISTS `issue`;
 
 CREATE TABLE `issue` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `from_location` int(11) DEFAULT NULL,
   `company_id` int(11) DEFAULT NULL,
   `depot_id` int(11) DEFAULT NULL,
   `created_by` int(11) DEFAULT NULL,
   `updated_by` int(11) DEFAULT NULL,
+  `finalized_by` int(11) DEFAULT NULL,
   `issue_date` date NOT NULL,
   `status` tinyint(1) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT NULL,
-  `finalized_by` int(11) DEFAULT NULL,
   `finalize_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
+  KEY `IDX_12AD233E18716951` (`from_location`),
   KEY `IDX_12AD233E979B1AD6` (`company_id`),
   KEY `IDX_12AD233E8510D4DE` (`depot_id`),
   KEY `IDX_12AD233EDE12AB56` (`created_by`),
   KEY `IDX_12AD233E16FE72E1` (`updated_by`),
   KEY `IDX_12AD233EDCA2A522` (`finalized_by`),
+  CONSTRAINT `FK_12AD233EDCA2A522` FOREIGN KEY (`finalized_by`) REFERENCES `users` (`id`),
   CONSTRAINT `FK_12AD233E16FE72E1` FOREIGN KEY (`updated_by`) REFERENCES `users` (`id`),
+  CONSTRAINT `FK_12AD233E18716951` FOREIGN KEY (`from_location`) REFERENCES `location` (`id`),
   CONSTRAINT `FK_12AD233E8510D4DE` FOREIGN KEY (`depot_id`) REFERENCES `depot` (`id`),
   CONSTRAINT `FK_12AD233E979B1AD6` FOREIGN KEY (`company_id`) REFERENCES `company` (`id`),
-  CONSTRAINT `FK_12AD233EDCA2A522` FOREIGN KEY (`finalized_by`) REFERENCES `users` (`id`),
   CONSTRAINT `FK_12AD233EDE12AB56` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `issue` */
 
-insert  into `issue`(`id`,`company_id`,`depot_id`,`created_by`,`updated_by`,`issue_date`,`status`,`created_at`,`updated_at`,`finalized_by`,`finalize_date`) values (1,1,1,1,NULL,'2014-08-05',0,'2014-08-05 17:55:39','2014-08-05 17:55:39',NULL,NULL),(2,1,1,1,NULL,'2014-08-07',0,'2014-08-07 09:56:34','2014-08-07 09:56:34',NULL,NULL),(3,1,1,1,1,'2014-08-09',1,'2014-08-09 15:55:36','2014-08-09 16:01:39',1,'2014-08-09 16:16:38'),(4,1,1,1,NULL,'2014-08-12',0,'2014-08-12 09:30:13',NULL,NULL,NULL),(5,1,1,1,NULL,'2014-08-12',0,'2014-08-12 09:43:07',NULL,NULL,NULL);
+insert  into `issue`(`id`,`from_location`,`company_id`,`depot_id`,`created_by`,`updated_by`,`finalized_by`,`issue_date`,`status`,`created_at`,`updated_at`,`finalize_date`) values (1,1,1,1,1,1,NULL,'2014-08-01',0,'2014-08-19 11:56:40','2014-08-19 12:01:07',NULL),(2,1,1,1,1,1,NULL,'2014-08-02',0,'2014-08-19 11:57:13','2014-08-19 12:01:18',NULL),(3,1,1,1,1,NULL,1,'2014-08-19',1,'2014-08-19 11:58:47','2014-08-19 11:58:51','2014-08-19 11:58:51'),(4,1,1,1,1,NULL,NULL,'2014-08-19',0,'2014-08-19 11:59:14',NULL,NULL),(5,1,1,1,1,NULL,1,'2014-08-19',1,'2014-08-19 11:59:35','2014-08-19 11:59:38','2014-08-19 11:59:38'),(6,1,1,1,1,NULL,1,'2014-08-19',1,'2014-08-19 12:00:02','2014-08-19 12:00:08','2014-08-19 12:00:08'),(7,1,1,1,1,NULL,1,'2014-08-19',1,'2014-08-19 12:00:24','2014-08-19 12:00:27','2014-08-19 12:00:27'),(8,1,1,1,1,NULL,1,'2014-08-19',1,'2014-08-19 12:00:50','2014-08-19 12:00:53','2014-08-19 12:00:53'),(9,1,1,1,1,NULL,1,'2014-08-05',1,'2014-08-19 12:01:53','2014-08-19 12:01:56','2014-08-19 12:01:56');
 
 /*Table structure for table `issue_line` */
 
@@ -86,17 +89,32 @@ CREATE TABLE `issue_line` (
   `issue_id` int(11) DEFAULT NULL,
   `product_id` int(11) DEFAULT NULL,
   `quantity` decimal(10,4) NOT NULL,
-  `comment` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `purpose` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `comment` longtext COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`),
   KEY `IDX_89F37C3A5E7AA58C` (`issue_id`),
   KEY `IDX_89F37C3A4584665A` (`product_id`),
   CONSTRAINT `FK_89F37C3A4584665A` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
   CONSTRAINT `FK_89F37C3A5E7AA58C` FOREIGN KEY (`issue_id`) REFERENCES `issue` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `issue_line` */
 
-insert  into `issue_line`(`id`,`issue_id`,`product_id`,`quantity`,`comment`) values (1,1,6,1.0000,'for mamun'),(2,2,2,1.0000,'jony'),(3,2,7,1.0000,'raju'),(4,2,3,1.0000,'zahed'),(5,3,1,1.0000,'dfsdf'),(6,3,5,1.0000,'sdsfsdf'),(7,4,4,1.0000,'ddd'),(8,5,6,1.0000,'for');
+insert  into `issue_line`(`id`,`issue_id`,`product_id`,`quantity`,`purpose`,`comment`) values (1,1,6,1.0000,'jony','this is a test'),(2,2,2,1.0000,'azad',''),(3,3,7,1.0000,'harun',''),(4,4,5,1.0000,'eon group','for all '),(5,5,5,1.0000,'eongroup','this is a test'),(6,6,3,3.0000,'azad,mamun,rony',''),(7,7,4,1.0000,'yousuf',''),(8,8,6,1.0000,'new','sdfsdf'),(9,9,6,1.0000,'rag','');
+
+/*Table structure for table `location` */
+
+DROP TABLE IF EXISTS `location`;
+
+CREATE TABLE `location` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `location` */
+
+insert  into `location`(`id`,`name`) values (1,'Eon Head Office');
 
 /*Table structure for table `product` */
 
@@ -106,7 +124,7 @@ CREATE TABLE `product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `product` */
 
@@ -118,32 +136,32 @@ DROP TABLE IF EXISTS `purchase`;
 
 CREATE TABLE `purchase` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `company_id` int(11) DEFAULT NULL,
-  `depot_id` int(11) DEFAULT NULL,
+  `supplier_id` int(11) DEFAULT NULL,
+  `location_id` int(11) DEFAULT NULL,
   `created_by` int(11) DEFAULT NULL,
   `updated_by` int(11) DEFAULT NULL,
+  `finalized_by` int(11) DEFAULT NULL,
   `status` smallint(6) NOT NULL,
   `purchase_date` date NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT NULL,
-  `finalized_by` int(11) DEFAULT NULL,
   `finalize_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `IDX_6117D13B979B1AD6` (`company_id`),
-  KEY `IDX_6117D13B8510D4DE` (`depot_id`),
+  KEY `IDX_6117D13B2ADD6D8C` (`supplier_id`),
+  KEY `IDX_6117D13B64D218E` (`location_id`),
   KEY `IDX_6117D13BDE12AB56` (`created_by`),
   KEY `IDX_6117D13B16FE72E1` (`updated_by`),
   KEY `IDX_6117D13BDCA2A522` (`finalized_by`),
-  CONSTRAINT `FK_6117D13B16FE72E1` FOREIGN KEY (`updated_by`) REFERENCES `users` (`id`),
-  CONSTRAINT `FK_6117D13B8510D4DE` FOREIGN KEY (`depot_id`) REFERENCES `depot` (`id`),
-  CONSTRAINT `FK_6117D13B979B1AD6` FOREIGN KEY (`company_id`) REFERENCES `company` (`id`),
   CONSTRAINT `FK_6117D13BDCA2A522` FOREIGN KEY (`finalized_by`) REFERENCES `users` (`id`),
+  CONSTRAINT `FK_6117D13B16FE72E1` FOREIGN KEY (`updated_by`) REFERENCES `users` (`id`),
+  CONSTRAINT `FK_6117D13B2ADD6D8C` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`id`),
+  CONSTRAINT `FK_6117D13B64D218E` FOREIGN KEY (`location_id`) REFERENCES `location` (`id`),
   CONSTRAINT `FK_6117D13BDE12AB56` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `purchase` */
 
-insert  into `purchase`(`id`,`company_id`,`depot_id`,`created_by`,`updated_by`,`status`,`purchase_date`,`created_at`,`updated_at`,`finalized_by`,`finalize_date`) values (1,1,1,1,NULL,1,'2014-08-05','2014-08-05 17:32:45','2014-08-05 17:32:45',NULL,NULL),(2,1,1,1,NULL,0,'2014-08-05','2014-08-05 17:54:17','2014-08-05 17:54:17',NULL,NULL),(3,1,1,1,1,1,'2014-08-09','2014-08-07 09:10:03','2014-08-07 09:10:03',1,NULL),(4,1,1,1,NULL,1,'2014-08-09','2014-08-09 07:50:25','2014-08-09 07:50:25',1,'2014-08-09 15:38:45'),(5,1,1,1,NULL,1,'2014-08-09','2014-08-09 11:00:52','2014-08-09 11:00:52',1,NULL),(6,1,1,1,NULL,1,'2014-08-09','2014-08-09 11:10:00','2014-08-09 11:10:00',1,'2014-08-09 15:19:00'),(7,1,1,1,1,1,'2014-08-09','2014-08-09 15:19:46','2014-08-09 15:21:26',1,'2014-08-09 15:22:23'),(8,1,1,1,NULL,0,'2014-08-12','2014-08-12 09:42:33',NULL,NULL,NULL);
+insert  into `purchase`(`id`,`supplier_id`,`location_id`,`created_by`,`updated_by`,`finalized_by`,`status`,`purchase_date`,`created_at`,`updated_at`,`finalize_date`) values (1,1,1,1,1,NULL,0,'2014-08-01','2014-08-19 11:36:36','2014-08-19 11:36:45',NULL),(2,1,1,1,NULL,NULL,0,'2014-08-02','2014-08-19 11:38:56',NULL,NULL),(3,1,1,1,NULL,NULL,0,'2014-08-03','2014-08-19 11:39:12',NULL,NULL),(4,1,1,1,NULL,1,1,'2014-08-04','2014-08-19 11:39:33','2014-08-19 11:39:37','2014-08-19 11:39:37'),(5,1,1,1,NULL,NULL,0,'2014-08-05','2014-08-19 11:39:52',NULL,NULL),(6,1,1,1,NULL,1,1,'2014-08-06','2014-08-19 11:40:09','2014-08-19 11:40:12','2014-08-19 11:40:12'),(7,1,1,1,NULL,NULL,0,'2014-08-08','2014-08-19 11:40:30',NULL,NULL),(8,1,1,1,NULL,1,1,'2014-08-09','2014-08-19 11:40:47','2014-08-19 11:40:50','2014-08-19 11:40:50'),(9,1,1,1,NULL,1,1,'2014-08-10','2014-08-19 11:41:13','2014-08-19 11:41:16','2014-08-19 11:41:16'),(10,1,1,1,NULL,1,1,'2014-08-12','2014-08-19 11:41:32','2014-08-19 11:41:35','2014-08-19 11:41:35'),(11,1,1,1,NULL,1,1,'2014-08-16','2014-08-19 11:41:48','2014-08-19 11:41:51','2014-08-19 11:41:51'),(12,1,1,1,NULL,1,1,'2014-08-17','2014-08-19 11:42:16','2014-08-19 11:42:19','2014-08-19 11:42:19');
 
 /*Table structure for table `purchase_line` */
 
@@ -158,13 +176,13 @@ CREATE TABLE `purchase_line` (
   PRIMARY KEY (`id`),
   KEY `IDX_A1A77C954584665A` (`product_id`),
   KEY `IDX_A1A77C95558FBEB9` (`purchase_id`),
-  CONSTRAINT `FK_A1A77C954584665A` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
-  CONSTRAINT `FK_A1A77C95558FBEB9` FOREIGN KEY (`purchase_id`) REFERENCES `purchase` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  CONSTRAINT `FK_A1A77C95558FBEB9` FOREIGN KEY (`purchase_id`) REFERENCES `purchase` (`id`),
+  CONSTRAINT `FK_A1A77C954584665A` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `purchase_line` */
 
-insert  into `purchase_line`(`id`,`product_id`,`purchase_id`,`quantity`,`price`) values (1,6,1,1.00,35000.00),(2,2,2,1.00,400.00),(3,1,3,1.00,300.00),(4,5,3,1.00,300.00),(5,7,4,1.00,30000.00),(6,3,3,1.00,3000.00),(7,2,5,1.00,200.00),(8,5,6,1.00,3000.00),(9,3,7,1.00,1000.00),(10,1,7,1.00,300.00),(11,6,8,1.00,200.00);
+insert  into `purchase_line`(`id`,`product_id`,`purchase_id`,`quantity`,`price`) values (1,6,1,1.00,100.00),(2,6,2,1.00,35000.00),(3,7,3,1.00,35000.00),(4,1,4,2.00,600.00),(5,5,5,2.00,6000.00),(6,3,6,3.00,9000.00),(7,4,7,5.00,5000.00),(8,6,8,2.00,90000.00),(9,2,9,1.00,300.00),(10,1,10,1.00,400.00),(11,1,11,3.00,900.00),(12,1,12,3.00,900.00),(13,2,12,2.00,800.00);
 
 /*Table structure for table `supplier` */
 
@@ -179,11 +197,11 @@ CREATE TABLE `supplier` (
   PRIMARY KEY (`id`),
   KEY `IDX_9B2A6C7EDE12AB56` (`created_by`),
   CONSTRAINT `FK_9B2A6C7EDE12AB56` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `supplier` */
 
-insert  into `supplier`(`id`,`created_by`,`name`,`address`,`created_at`) values (1,NULL,'Computer Source Ltd','House: 11/B, Road: 12 (New) 31 (Old)\r\nDhanmondi R/A, Dhaka-1209\r\n\r\nTel: +880-2-9127592, +880-2-9121484\r\n+880-2-9140152, \r\n+880-2-9145688, +880-2-8120578','2014-08-17 18:07:05'),(2,NULL,'testa','sdfsdf','2014-08-17 18:08:09');
+insert  into `supplier`(`id`,`created_by`,`name`,`address`,`created_at`) values (1,NULL,'Computer Source Ltd','House: 11/B, Road: 12 (New) 31 (Old)\r\nDhanmondi R/A, Dhaka-1209\r\n\r\nTel: +880-2-9127592, +880-2-9121484\r\n+880-2-9140152, \r\n+880-2-9145688, +880-2-8120578','2014-08-17 18:07:05'),(2,NULL,'testa','sdfsdf','2014-08-17 18:08:09'),(5,NULL,'test3','test3','2014-08-18 16:27:20');
 
 /*Table structure for table `users` */
 
@@ -210,7 +228,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_1483A5E992FC23A8` (`username_canonical`),
   UNIQUE KEY `UNIQ_1483A5E9A0D96FBF` (`email_canonical`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `users` */
 
