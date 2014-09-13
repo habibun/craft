7f7d5f16e3c3ba45e793765be739557c2e0f6a77
Habibun Noby <habibun.noby@gmail.com>
2014-09-13 12:40:51 +0600

    added custom twig extension

---
+++ src/Acme/UserBundle/Twig/MyTwigExtension.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Habibun
+ * Date: 9/13/14
+ * Time: 12:05 PM
+ */
+namespace Acme\UserBundle\Twig;
+
+use Twig_Extension;
+use Twig_Filter_Method;
+
+class MyTwigExtension extends Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            'camelize' => new Twig_Filter_Method($this, 'camelizeFilter'),
+        );
+    }
+
+    public function camelizeFilter($value)
+    {
+        if(!is_string($value)) {
+            return $value;
+        }
+
+        $chunks    = explode(' ', $value);
+        $ucfirsted = array_map(function($s) { return ucfirst($s); }, $chunks);
+
+        return implode('', $ucfirsted);
+    }
+
+    public function getName()
+    {
+        return 'my_twig_extension';
+    }
+}
