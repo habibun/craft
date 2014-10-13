c6209b41db79b5ea9ba47422167841b4f550b363
Habibun Noby <habibun.noby@gmail.com>
2014-10-13 11:12:04 +0600

    product history status condition added

--- src/Acme/HistoryBundle/Controller/ProductHistoryController.php
+++ src/Acme/HistoryBundle/Controller/ProductHistoryController.php
@@ -27,9 +27,11 @@ class ProductHistoryController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $query = $em->createQuery(
-            'SELECT IDENTITY(pl.product), sum(pl.quantity) as total_product_in,pr.name as name
+            'SELECT IDENTITY(pl.product), sum(pl.quantity) as total_product_in,pr.name as name, pu.status as active
              from AcmePurchaseBundle:PurchaseLine pl
+             join pl.purchase pu
              join pl.product pr
+             where pu.status = 1
              group by pl.product
              order by total_product_in DESC
              '
@@ -54,9 +56,11 @@ class ProductHistoryController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $query = $em->createQuery(
-            'SELECT IDENTITY(i.product), sum(i.quantity) as total_product_out,pro.name as name
+            'SELECT IDENTITY(i.product), sum(i.quantity) as total_product_out, pro.name as name, iss.status as active
              from AcmeIssueBundle:IssueLine i
+             join i.issue iss
              join i.product pro
+             where iss.status = 1
              group by i.product
              order by total_product_out DESC
              '
