7b47512d1248a8b303b1de6ab6a0481222f85811
Habibun Noby <habibun.noby@gmail.com>
2014-10-14 14:46:11 +0600

    remove all function

--- src/Acme/PurchaseBundle/Repository/PurchaseRepository.php
+++ src/Acme/PurchaseBundle/Repository/PurchaseRepository.php
@@ -11,16 +11,4 @@ use Acme\PurchaseBundle\Entity\Purchase;
 class PurchaseRepository extends EntityRepository
 {
 
-    public function _finalize($id)
-    {
-        $purchase = new Purchase();
-        $purchase->setStatus(1);
-        $purchase->setFinalizedBy('jony');
-        $purchase->setFinalizedAt(new \DateTime('now'));
-        $this->_em->persist($purchase);
-        $this->_em->flush();
-
-        return true;
-    }
-
 }
