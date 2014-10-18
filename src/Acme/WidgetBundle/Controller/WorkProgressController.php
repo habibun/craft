7daae60a1bb53264d20c56821087c061fdbf311e
Habibun Noby <habibun.noby@gmail.com>
2014-10-18 13:34:31 +0600

    reformat code

--- src/Acme/WidgetBundle/Controller/WorkProgressController.php
+++ src/Acme/WidgetBundle/Controller/WorkProgressController.php
@@ -10,30 +10,36 @@ namespace Acme\WidgetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class WorkProgressController extends Controller{
+class WorkProgressController extends Controller
+{
 
     public function workProgressAction()
     {
         $workProgress = $this->_getWorkProgressResults();
-        return $this->render('AcmeWidgetBundle:WorkProgress:workProgress.html.twig',array(
-            'workProgress' => $workProgress    
-            ));
+
+        return $this->render(
+            'AcmeWidgetBundle:WorkProgress:workProgress.html.twig',
+            array(
+                'workProgress' => $workProgress
+            )
+        );
     }
 
     private function _getWorkProgressResults()
     {
-    	$em = $this->getDoctrine()->getManager();
-    	$query = $em->createQuery(
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
             'SELECT u.username as name, 
-            p.id id,
-            count(p.createdBy) AS total_created, 
-            count(p.updatedBy) as total_updated,
-            count(p.finalizedBy) as total_finalized
+             p.id id,
+             count(p.createdBy) AS total_created,
+             count(p.updatedBy) as total_updated,
+             count(p.finalizedBy) as total_finalized
              FROM AcmePurchaseBundle:Purchase p
              JOIN p.createdBy u
              group by name'
         );
         $result = $query->getResult();
+
         return $result;
     }
 } 
