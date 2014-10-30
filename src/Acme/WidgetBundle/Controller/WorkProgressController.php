5214ab9779b864336d23d19c8a69bbf68b757058
habibun <habibun.noby@gmail.com>
2014-10-31 01:14:24 +0600

    workprogess fix

--- src/Acme/WidgetBundle/Controller/WorkProgressController.php
+++ src/Acme/WidgetBundle/Controller/WorkProgressController.php
@@ -18,16 +18,15 @@ class WorkProgressController extends Controller
         $username = $this->get('security.context')->getToken()->getUser();
         $em = $this->getDoctrine()->getManager();
         $query = $em->createQuery(
-                        'SELECT sum(p.createdBy) as name FROM
-                        AcmePurchaseBundle:Purchase p
-                        join p.createdBy u
-                        WHERE u.username = :username')
+                        'SELECT sum(p.createdBy) as total_created, sum(p.updatedBy) as total_updated, sum(p.finalizedBy) as total_finalized
+                         FROM AcmePurchaseBundle:Purchase p
+                         join p.createdBy u
+                         WHERE u.username = :username')
                     ->setParameters(
                             array('username' => $username->getUsername())
                         );
                     
         $workProgress= $query->getOneOrNullResult();
-
 
         return $this->render(
             'AcmeWidgetBundle:WorkProgress:workProgress.html.twig',
@@ -36,24 +35,6 @@ class WorkProgressController extends Controller
                 
             )
         );
-    }
-
-    private function _getWorkProgressResults()
-    {
-        $em = $this->getDoctrine()->getManager();
-        $query = $em->createQuery(
-            'SELECT u.username as name, 
-             p.id id,
-             count(p.createdBy) AS total_created,
-             count(p.updatedBy) as total_updated,
-             count(p.finalizedBy) as total_finalized
-             FROM AcmePurchaseBundle:Purchase p
-             JOIN p.createdBy u
-             group by name'
-        );
-        $result = $query->getResult();
-
-        return $result;
     }
 } 
 
