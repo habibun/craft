acdc582905749c3322af93696971fa8732a6005e
Habibun Noby <habibun.noby@gmail.com>
2014-11-08 09:55:06 +0600

    remove deprecated function

--- src/Acme/WidgetBundle/Controller/SearchStatisticsController.php
+++ src/Acme/WidgetBundle/Controller/SearchStatisticsController.php
@@ -11,72 +11,60 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
-class SearchStatisticsController extends Controller{
+class SearchStatisticsController extends Controller
+{
 
     public function searchAction($searchWord)
     {
         $result = array();
-
         $search = trim($searchWord);
-
         if (strlen($search)) {
-
-            $repository = $this->getDoctrine()
-                ->getManager()
-                ->getRepository('AcmeWidgetBundle:SearchStatistics');
-
+            $repository = $this->getDoctrine()->getManager()->getRepository('AcmeWidgetBundle:SearchStatistics');
             $result = $repository->getGeneralSearchResults(strtolower($search));
-
             //save statistics
             $repository->saveSerchRequest($search);
         }
         $paginator = $this->get('knp_paginator');
         $result = $paginator->paginate(
             $result,
-            $this->get('request')->query->get('page', 1) /*page number*/,
-            10
-        /*limit per page*/
+            $this->get('request')->query->get('page', 1)
+            /*page number*/,
+            10/*limit per page*/
         );
 
         return $this->render(
             'AcmeWidgetBundle:SearchStatistics:searchResult.html.twig',
-            array(
-                'result' => $result,
-                'search' => $search,
-            )
+            array('result' => $result, 'search' => $search,)
         );
     }
 
-    public function autocompleteAction(Request $request){
-
-        $search = strtolower( trim($request->get('term') ));
-        
+    public function autocompleteAction(Request $request)
+    {
+        $search = strtolower(trim($request->get('term')));
         $result = array();
-
         if (strlen($search) > 2) {
-
-            $result = $this->getDoctrine()
-                    ->getEntityManager()
-                    ->getRepository('AcmeWidgetBundle:SearchStatistics')
-                    ->getSuggestSearchResults($search);
+            $result = $this->getDoctrine()->getManager()->getRepository(
+                'AcmeWidgetBundle:SearchStatistics'
+            )->getSuggestSearchResults($search);
         }
 
         $response = new Response(json_encode($result));
         $response->headers->set('Content-Type', 'application/json');
 
         return $response;
-    }    
+    }
 
     public function topSearchListAction()
     {
-        //getting top 6 searched keywords from DB
-        $words = $this->getDoctrine()
-                      ->getManager()
-                      ->getRepository("AcmeWidgetBundle:SearchStatistics")
-                      ->getTopSearchedKeyWords(6);
+        $words = $this->getDoctrine()->getManager()->getRepository(
+            "AcmeWidgetBundle:SearchStatistics"
+        )->getTopSearchedKeyWords(6);
 
-        return $this->render('AcmeWidgetBundle:SearchStatistics:topSearchList.html.twig', array(
+        return $this->render(
+            'AcmeWidgetBundle:SearchStatistics:topSearchList.html.twig',
+            array(
                 'words' => $words,
-            ));                      
-        }
+            )
+        );
+    }
 }
