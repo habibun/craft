985c4b1f8448475696e0d75338ac8defe597af64
habibun <habibun.noby@gmail.com>
2014-11-28 01:52:43 +0600

    remove extra route

--- src/Acme/WidgetBundle/Controller/SearchStatisticsController.php
+++ src/Acme/WidgetBundle/Controller/SearchStatisticsController.php
@@ -67,12 +67,4 @@ class SearchStatisticsController extends Controller
             )
         );
     }
-
-    public function testAction()
-    {
-        return $this->render(
-            'AcmeWidgetBundle:SearchStatistics:_issueResult.html.twig'
-            
-        );
-    }
 }
