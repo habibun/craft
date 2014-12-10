e5173bce191108a8e21ea66218d749718db8e09d
Habibun Noby <habibun.noby@gmail.com>
2014-12-10 18:08:28 +0600

    try available product

--- src/Acme/WidgetBundle/Controller/AvailableProductController.php
+++ src/Acme/WidgetBundle/Controller/AvailableProductController.php
@@ -6,8 +6,49 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AvailableProductController extends Controller
 {
-	public function availableProductAction()
-	{
-		return $this->render('AcmeWidgetBundle:AvailableProduct:availableProduct.html.twig');
-	}
+    public function availableProductAction()
+    {
+        $ap = $this->_getAvailableProductResult();
+        var_dump($ap);
+
+        return $this->render('AcmeWidgetBundle:AvailableProduct:index.html.twig',array(
+                'ap' => $ap
+            ));
+    }
+
+    private function _getAvailableProductResult()
+    {
+        $query = $this->getEntityManager($this->em)
+            ->createQueryBuilder('u')
+            ->select(
+                'u.ratingkey, u.origTitle, u.origTitleEp, u.episode, u.season, u.year, u.xml, count(u.title) as playCount'
+            )
+            ->from($this->class, 'u')
+            ->groupBy('u.title')
+            ->orderBy('playCount', 'desc')
+            ->addOrderBy('u.ratingkey', 'desc')
+            ->setMaxResults(10)
+            ->getQuery();
+
+        return $query->getResult();
+    }
+
+    private function testResult()
+    {
+        $q = Doctrine_Query::create()
+            ->select('count(*)')
+            ->from('mbScoreByGenre s')
+            ->where('s.parent_genre_id = ?', $genre['parent_id'])
+            ->having(
+                "SUM(s.score) > (
+    SELECT SUM(p.score)
+    FROM mbScoreByGenre p
+    WHERE p.parent_genre_id = {$genre['parent_id']}
+      AND p.user_id = {$user_id})"
+            )
+            ->groupBy('s.user_id');
+
+        $result = $q->execute(array(), Doctrine::HYDRATE_SCALAR);
+        var_dump($result);
+    }
 }
