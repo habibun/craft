d39101bb308c2a02439676d48fae967c8d9d37ca
habibun <habibun.noby@gmail.com>
2014-12-12 21:26:32 +0600

    profile edit view change

--- src/Acme/UserBundle/Form/Type/ProfileFormType.php
+++ src/Acme/UserBundle/Form/Type/ProfileFormType.php
@@ -42,12 +42,10 @@ class ProfileFormType extends AbstractType
 
         $builder->add('current_password', 'password', array(
             'label' => 'form.current_password',
-            'translation_domain' => 'FOSUserBundle',
+            'translation_domain' => 'AcmeUserBundle',
             'mapped' => false,
             'constraints' => $constraint,
-        ))
-        ->add('file');
-        ;
+        ));
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
@@ -72,9 +70,9 @@ class ProfileFormType extends AbstractType
     protected function buildUserForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
-            ->add('email', 'email', array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
-            ->add('file', 'file', array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
+            ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'AcmeUserBundle'))
+            ->add('email', 'email', array('label' => 'form.email', 'translation_domain' => 'AcmeUserBundle'))
+            ->add('file', 'file', array('label' => 'form.file', 'translation_domain' => 'AcmeUserBundle'))
         ;
     }
 }
