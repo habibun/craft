26fd1c633a6a9205318d782ac50ba9344df7ab40
habibun <habibun.noby@gmail.com>
2014-12-16 01:07:54 +0600

    genemu form bundle added

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -35,6 +35,7 @@ class AppKernel extends Kernel
             new Cg\KintBundle\CgKintBundle(),
             new Ali\DatatableBundle\AliDatatableBundle(),
             new Acme\EmailBundle\AcmeEmailBundle(),
+            new Genemu\Bundle\FormBundle\GenemuFormBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
